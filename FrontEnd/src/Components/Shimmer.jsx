00428766d445233a881ea813acd97ab42faae419
Xenon1202 <MOHITMISHRAK47@GMAIL.COM>
2025-07-31 16:49:38 +0530

    Few Shimmer changes

--- FrontEnd/src/Components/Shimmer.jsx
+++ FrontEnd/src/Components/Shimmer.jsx
@@ -1,8 +1,10 @@
 const ShimmerUi = () => {
   return (
     <>
-      <div className=" w-[500px] h-[400px] bg-white flex m-3 p-3 rounded-md"></div>
-      <div className=" w-[500px] h-[400px] bg-white flex m-3 p-3 rounded-md"></div>
+    <div className="card-container flex flex-wrap justify-center">
+      <div className=" w-[500px] h-[200px] bg-white flex m-3 p-3 rounded-md"></div>
+      <div className=" w-[500px] h-[200px] bg-white flex m-3 p-3 rounded-md"></div>
+    </div>
     </>
   );
 };
